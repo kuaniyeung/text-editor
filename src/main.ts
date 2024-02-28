c00ee937108c188d46823b58c73bb7ebd03f8282
kuaniyeung@gmail.com <kuaniyeung@gmail.com>
2024-02-27 19:26:47 -0500

    Add basic typing functionalities and display function to worker

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,21 @@ import Worker from "./worker?worker";
 
 const worker = new Worker();
 const canvas = document.body.appendChild(document.createElement("canvas"));
-canvas.id = "canvas"
+canvas.id = "canvas";
 const context = canvas.transferControlToOffscreen();
+const fontFile = new FontFace(
+  "jetbrains-mono",
+  "url(https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap)"
+);
 
-worker.postMessage(["setUpCanvas", context], [context]);
 
-addEventListener("keydown", (e) => worker.postMessage([e.type, e.key]));
+fontFile.loaded.then(
+  () => {
+    document.fonts.add(fontFile);
+    worker.postMessage(["setUpCanvas", context], [context]);
+    addEventListener("keydown", (e) => worker.postMessage([e.type, e.key]));
+  },
+  (err) => {
+    console.error(err);
+  }
+);
--- src/worker.ts
+++ src/worker.ts
@@ -1,113 +1,131 @@
-let size = 50;
+let size = 10;
 let buffer = new Array(size);
 
 let gap_size = 10;
 let gap_left = 0;
 let gap_right = gap_size - gap_left - 1;
 
-let ctx;
-let canvas;
+let ctx: CanvasRenderingContext2D;
+let canvas: HTMLCanvasElement;
+let cursorPosition;
 let eventKey;
-let page;
+let value;
+
+// Function that is used to grow the gap at index position and return the array
+const grow = (k: number, position: number) => {
+  let a = buffer.slice(position, gap_size);
+  // Copy characters of buffer to a[] after position
+  buffer.splice(position, gap_size - position, ...Array(k).fill(undefined));
+  // Insert a gap of k from index position
+  // Reinsert the remaining array
+  buffer.splice(position + k, 0, ...a);
+  gap_size += k;
+  gap_right += k;
+};
+
+// Function that is used to move the gap left and right in the array
+const moveGap = (direction: string) => {
+  if (direction === "left") {
+    gap_left--;
+    gap_right--;
+    buffer[gap_right + 1] = buffer[gap_left];
+    buffer[gap_left] = undefined;
+  } else if (direction === "right") {
+    gap_left++;
+    gap_right++;
+    buffer[gap_left - 1] = buffer[gap_right];
+    buffer[gap_right] = undefined;
+  }
+};
+
+// Function to insert the string to the buffer at point position
+const insert = (input: string) => {
+  // If the gap is empty grow the size
+  if (gap_right === gap_left) {
+    let k = 10;
+    grow(k, gap_left);
+  }
+
+  // Insert the character in the gap and move the gap
+  buffer[gap_left] = input;
+  gap_left++;
+};
+
+const remove = () => {
+  gap_left--;
+  buffer[gap_left] = undefined;
+};
+
+const display = () => {
+  const span = 24 / (10 / 6);
+  ctx.fillStyle = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+  // clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.width);
+
+  const aBuffer = buffer.slice(0, gap_left).join("");
+  const bBuffer = buffer.slice(gap_right).join("");
+
+  // render value before cursor
+  ctx.fillText(aBuffer, 0, 0);
+
+  // render cursor
+  ctx.fillRect((cursorPosition = span * aBuffer.length - 1), 0, 1, 24);
+
+  // render value after cursor
+  ctx.fillText(bBuffer, span * aBuffer.length, 0);
+};
 
 // Worker messages
 onmessage = (e) => {
-  if (e.data[0] === "keydown") {
-    eventKey = e.data[1];
-
-    console.log(eventKey);
-  }
-
   if (e.data[0] === "setUpCanvas") {
     // define canvas from main to offscreen
     canvas = e.data[1];
-    ctx = canvas.getContext("2d");
+    ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
     // canvas size
     canvas.width = 500;
     canvas.height = 500;
 
     // font style
-    ctx.font = "24px monospace";
+    ctx.font = "24px jetbrains-mono";
     ctx.textBaseline = "top";
 
     // ctx.fillText("abcde", 0, 0);
   }
+
+  if (e.data[0] === "keydown") {
+    eventKey = e.data[1];
+
+    switch (eventKey) {
+      case "Shift":
+      case "Control":
+      case "Alt":
+      case "Meta":
+      case "Tab":
+      case "Fn":
+      case "ArrowUp":
+      case "ArrowDown":
+        break;
+      case "ArrowLeft":
+        moveGap("left");
+        break;
+      case "ArrowRight":
+        moveGap("right");
+        break;
+      case "Backspace":
+        remove();
+        break;
+      case "Enter":
+        value = "\n";
+        insert(value);
+        break;
+      default:
+        insert(eventKey);
+    }
+
+    // console.log(eventKey);
+  }
+  display();
+  console.log(buffer, gap_left, gap_right, gap_size);
 };
-
-console.log(buffer);
-// Function that is used to grow the gap at index position and return the array
-// function grow(k: number, position: number) {
-//   let a = buffer.slice(position, size);
-//   // Copy characters of buffer to a[] after position
-//   buffer.splice(position, size - position, ..."_".repeat(k));
-//   // Insert a gap of k from index position
-//   // gap is being represented by '_'
-//   // Reinsert the remaining array
-//   buffer.splice(position + k, 0, ...a);
-//   size += k;
-//   gap_right += k;
-// }
-
-// // Function that is used to move the gap left in the array
-// function left(position: number) {
-//   // Move the gap left character by character and the buffers
-//   while (position < gap_left) {
-//     gap_left--;
-//     gap_right--;
-//     buffer[gap_right + 1] = buffer[gap_left];
-//     buffer[gap_left] = "_";
-//   }
-// }
-// // Function that is used to move the gap right in the array
-// function right(position: number) {
-//   // Move the gap right character by character and the buffers
-//   while (position > gap_left) {
-//     gap_left++;
-//     gap_right++;
-//     buffer[gap_left - 1] = buffer[gap_right];
-//     buffer[gap_right] = "_";
-//   }
-// }
-
-// // Function to control the movement of gap by checking its position to the point of insertion
-// // It checks whether the specified position is to the left or right of the current position of the gap, and then adjusts the gap accordingly.
-// function move_cursor(position: number) {
-//   if (position < gap_left) {
-//     left(position);
-//   } else {
-//     right(position);
-//   }
-// }
-
-// // Function to insert the string to the buffer at point position
-// function insertion(input: string, position: number) {
-//   let inputLength = input.length;
-//   let i = 0;
-//   // If the point is not the gap, check and move the cursor to that point
-//   if (position !== gap_left) {
-//     move_cursor(position);
-//   }
-
-//   // Insert characters one by one
-//   while (i < inputLength) {
-//     // If the gap is empty grow the size
-//     if (gap_right === gap_left) {
-//       let k = 10;
-//       grow(k, position);
-//     }
-//     // Insert the character in the gap and move the gap
-//     buffer[gap_left] = input.charAt(i);
-//     gap_left++;
-//     i++;
-//     position++;
-//   }
-// }
-
-// function deletion(position: number) {
-//   // Deleting a character at a given position
-//   if (position + 1 !== gap_left) move_cursor(position + 1);
-
-//   gap_left--;
-//   buffer[gap_left] = "_";
-// }
